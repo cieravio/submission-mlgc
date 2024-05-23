7322b27f5d539325c0578f67ea00c54c90914d57
cieravio <102254686+cieravio@users.noreply.github.com>
2024-05-23 09:44:56 +0700

    Add files via upload

---
+++ src/services/inferenceService.js
@@ -0,0 +1,28 @@
+const tf = require("@tensorflow/tfjs-node");
+
+const InputError = require("../exceptions/InputError.js");
+async function predictClassification(model, image) {
+  try {
+    const tensor = tf.node.decodeJpeg(image).resizeNearestNeighbor([224, 224]).expandDims().toFloat();
+
+    const prediction = model.predict(tensor);
+
+    const score = await prediction.data();
+
+    let label, suggestion;
+
+    if (score[0] > 0.5) {
+      label = "Cancer";
+      suggestion = "gws";
+    } else {
+      label = "Non-cancer";
+      suggestion = "Anda tidak terjangkit kanker";
+    }
+
+    return { label, suggestion };
+  } catch (error) {
+    throw new InputError("Terjadi kesalahan dalam melakukan prediksi");
+  }
+}
+
+module.exports = predictClassification;
